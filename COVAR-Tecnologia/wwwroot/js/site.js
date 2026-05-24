2403b33cd0d740b63a3da27cd7fa2f63ec0c6265
N4TSUg <enriquearana1402@gmail.com>
2026-05-24 14:34:25 -0500

    Mejora de pestañas de gestion

--- COVAR-Tecnologia/wwwroot/js/site.js
+++ COVAR-Tecnologia/wwwroot/js/site.js
@@ -28,3 +28,18 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('theme', newTheme);
     });
 });
+// Función para alternar visibilidad de contraseña
+function togglePassword(button) {
+    const input = button.previousElementSibling;
+    const icon = button.querySelector('i');
+    
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('bi-eye');
+        icon.classList.add('bi-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.remove('bi-eye-slash');
+        icon.classList.add('bi-eye');
+    }
+}
