8a9582c2e93d8df466f9c4e7ea3490e32dff1549
N4TSUg <enriquearana1402@gmail.com>
2026-06-08 13:27:18 -0500

    Eliminación de contraseñas y cadenas de conexion y eliminación de migraciones

--- COVAR-Tecnologia/Data/CoTecDBContext.cs
+++ COVAR-Tecnologia/Data/CoTecDBContext.cs
@@ -69,8 +69,8 @@ namespace COVAR_Tecnologia.Data
                 {
                     Id = 1,
                     Email = "enriquearana1402@gmail.com",
-                    // IMPORTANTE: Aquí va el hash, no la contraseña real "3nriqueA1402"
-                    Password = "$2a$12$tzU3g/s8DF2nU3Wu4t5sRuJ0j4jmhmrG0FZBhvadEgNe2Z0PUQnjq",
+                    // IMPORTANTE: Aquí va el hash, no la contraseña real "examplepass"
+                    Password = "", //Aquí va el hash de tu contraseña
                     RolId = 1 // 1 corresponde a Administrador
                 }
             );
